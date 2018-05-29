4d013ab5d467f1ed2ea987215301e579824b4d60
lchonghua <chonghua.li@gmail.com>
2018-05-29 13:26:40 -0500

    Refactor: add DTO (data transfer object) class. This DTO model is used to transfer data from client to server or from server to client.
    The original APIs receive or return a Customer object , which is part of the domain model (considered as implementation details, which can change frequently, and these changes can break existing clients that depend on the Customer object). Therefore we need to make the contract of these apis as stable as possible.
    
    Secondly, by receiving or returning domain objects, we are opening a security hole. Hacker can easily pass additional data into json and it will be mapped to domain object, even for properties that should not be updated. By using a DTO, you can simply exclude the properties that willnot be updated.

---
+++ MovieRentalApp/Dtos/Customer.cs
@@ -0,0 +1,30 @@
+﻿using MovieRentalApp.Models;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace MovieRentalApp.Dtos
+{
+    public class Customer
+    {
+        // copy the properties from Customer model
+        // modify the properties: they should be either primitive types or custom DTOs
+        public int Id { get; set; }
+
+        [Required]
+        [StringLength(255)]
+        public string Name { get; set; }
+
+        public bool IsSubscribedToNewsletter { get; set; }
+
+        
+        //public MembershipType MembershipType { get; set; }  exclude MembershipType, b/c this is a domain class, so it creates a dependency of this DTO to a domain model
+
+        public int MembershipTypeId { get; set; }
+
+        [Min18YearsIfAMember]
+        public DateTime? Birthday { get; set; } = null;
+    }
+}
